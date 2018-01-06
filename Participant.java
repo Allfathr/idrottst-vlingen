3fa1369adff92f1e18037e8f2a6d29846c1485f5
jesper1996 <35168070+jesper1996@users.noreply.github.com>
2018-01-06 17:57:16 +0100

    Update Participant.java

--- Participant.java
+++ Participant.java
@@ -1,15 +1,15 @@
 
 public class Participant {
 	
-	private String firstName;
-	private String lastName;
-	private String teamName;
+	private String firstName, lastName, teamName;
+	private int participantNumber;
 	
 	public Participant(String firstName, String lastName, String teamName) {
 	
 	this.firstName = firstName; 
 	this.lastName = lastName;
 	this.teamName = teamName;
+	this.participantNumber = participantNumber;
 	
 			
 	}
