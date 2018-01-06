d28f2449507f2f5ed88463b0aeb28fe6f34386ff
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-01-06 15:52:29 +0100

    Update SportsMethods.java

--- SportsMethods.java
+++ SportsMethods.java
@@ -7,23 +7,26 @@ public class SportsMethods {
 	private ArrayList<Event> allEvents = new ArrayList<>();
 	
 	public void addEvent() {
+		
 		System.out.print("Event name: ");
 		String nameInput = scan.nextLine();
 		String name = nameInput.substring(0,1).toUpperCase() + nameInput.substring(1).toLowerCase();
 		name = name.trim();
-		
-		if (allEvents.contains(name)) {
-			System.out.println("Error " + name + " already added.");
+			
+		while (name == null || name.isEmpty()) {
+			System.out.println("Error: name can't be empty." );
+			System.out.print("Event name: ");				
+			nameInput = scan.nextLine();
+			name = nameInput.substring(0,1).toUpperCase() + nameInput.substring(1).toLowerCase();
+			name = name.trim();
 		}
-
+			
 		System.out.print("Attempts allowed: ");
 		int attempts = scan.nextInt();
 		scan.nextLine();
 		Event event = new Event(name, attempts);
-		
+			
 		allEvents.add(event);
 		System.out.println(name + " added.");
 	}
-	
-
 }
