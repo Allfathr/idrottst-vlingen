06e6eaaa638846f228ba789cd8e099fd130db3c2
jesper1996 <35168070+jesper1996@users.noreply.github.com>
2018-01-06 16:42:16 +0100

    Add files via upload

---
+++ Participant.java
@@ -0,0 +1,17 @@
+
+public class Participant {
+	
+	private String firstName;
+	private String lastName;
+	private String teamName;
+	
+	public Participant(String firstName, String lastName, String teamName) {
+	
+	this.firstName = firstName; 
+	this.lastName = lastName;
+	this.teamName = teamName;
+	
+			
+	}
+
+}
