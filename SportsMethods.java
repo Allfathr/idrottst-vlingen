1e95a7f5f5e0404449a92701a3eb6761d9841549
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-01-06 16:13:04 +0100

    Update SportsMethods.java

--- SportsMethods.java
+++ SportsMethods.java
@@ -5,27 +5,39 @@ public class SportsMethods {
 	
 	private Scanner scan = new Scanner(System.in);
 	private ArrayList<Event> allEvents = new ArrayList<>();
+	private int attempts;
+	private String nameInput, name;
 	
 	public void addEvent() {
 		
-		System.out.print("Event name: ");
-		String nameInput = scan.nextLine();
-		String name = nameInput.substring(0,1).toUpperCase() + nameInput.substring(1).toLowerCase();
-		name = name.trim();
-			
 		while (name == null || name.isEmpty()) {
-			System.out.println("Error: name can't be empty." );
-			System.out.print("Event name: ");				
+			System.out.print("Event name: ");
 			nameInput = scan.nextLine();
 			name = nameInput.substring(0,1).toUpperCase() + nameInput.substring(1).toLowerCase();
 			name = name.trim();
+				
+			if (name == null || name.isEmpty()) {
+				System.out.println("Error: name can't be empty." );
+			}
+			else {
+				continue;
+			}
 		}
+		
+		while (attempts <= 0) {
+			System.out.print("Attempts allowed: ");
+			attempts = scan.nextInt();
+			scan.nextLine();
 			
-		System.out.print("Attempts allowed: ");
-		int attempts = scan.nextInt();
-		scan.nextLine();
+			if (attempts <= 0) {
+				System.out.println("Error: too low, must allow at least one attempt.");
+			}
+			else {
+				continue;
+			}
+		}
+		
 		Event event = new Event(name, attempts);
-			
 		allEvents.add(event);
 		System.out.println(name + " added.");
 	}
