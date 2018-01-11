3f0fe70480d610226137b4fd1d228d3cc265f2e7
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-01-11 16:49:25 +0100

    Update SportsMethods.java

--- SportsMethods.java
+++ SportsMethods.java
@@ -6,9 +6,11 @@ public class SportsMethods {
 	private Scanner scan = new Scanner(System.in);
 	private ArrayList<Event> allEvents = new ArrayList<>();
 	private ArrayList<String> eventNames = new ArrayList<>();
+	private ArrayList<Integer> participantNumbers = new ArrayList<>();
 	private ArrayList<Participant> allParticipants = new ArrayList<>();
-	private int attempts, participantNumber, resultNumber;
+	private int attempts, participantNumber, removeNumber, resultNumber, checkParticipant, searchParticipant;
 	private String eventName, firstName, lastName, teamName;
+	private int incrementNumber = 100;
 	
 	private String capitalizeTrim(String original) {
 		original = original.trim();
@@ -29,37 +31,35 @@ public class SportsMethods {
 			
 			else if (eventNames.contains(eventName = capitalizeTrim(eventName))) {
 				System.out.println("Error: event already exists." );
-				continue;
+				break;
 			}
 			
 			else {
 				eventName = capitalizeTrim(eventName);
-				break;
+				do {
+					System.out.print("Attempts allowed: ");
+					attempts = scan.nextInt();
+					scan.nextLine();
+					
+					if (attempts <= 0) {
+						System.out.println("Error: too low, must allow at least one attempt.");
+						continue;
+					}
+					else {
+						Event event = new Event(eventName, attempts);
+						allEvents.add(event);
+						eventNames.add(eventName);
+						System.out.println(eventName + " added.");
+						break;
+					}
+				} while (attempts <= 0);
 			}
 		} while (eventName == null || eventName.isEmpty());
-		
-		do {
-			System.out.print("Attempts allowed: ");
-			attempts = scan.nextInt();
-			scan.nextLine();
-			
-			if (attempts <= 0) {
-				System.out.println("Error: too low, must allow at least one attempt.");
-			}
-			else {
-				break;
-			}
-		}  while (attempts <= 0);
-	
-		Event event = new Event(eventName, attempts);
-		allEvents.add(event);
-		eventNames.add(eventName);
-		System.out.println(eventName + " added.");
 	}
 	
 	public void addParticipant() {
 		
-		while (firstName == null || firstName.isEmpty()) {
+		do {
 			System.out.print("First name: ");
 			firstName = scan.nextLine();
 			
@@ -71,8 +71,9 @@ public class SportsMethods {
 				firstName = capitalizeTrim(firstName);
 				break;
 			}
-		}
-		while (lastName == null || lastName.isEmpty()) {
+		} while (firstName == null || firstName.isEmpty());
+		
+		do {
 			System.out.print("Last name: ");
 			lastName = scan.nextLine();
 			
@@ -84,8 +85,9 @@ public class SportsMethods {
 				lastName = capitalizeTrim(lastName);
 				break;
 			}
-		}
-		while (teamName == null || teamName.isEmpty()) {
+		} while (lastName == null || lastName.isEmpty());
+		
+		do {
 			System.out.print("Team: ");
 			teamName = scan.nextLine();
 			
@@ -97,29 +99,97 @@ public class SportsMethods {
 				teamName = capitalizeTrim(teamName);
 				break;
 			}
-		}
+		} while (teamName == null || teamName.isEmpty());
+		
+		participantNumber = incrementNumber;
+		incrementNumber++;
 		Participant participant = new Participant(firstName, lastName, teamName, participantNumber);
 		allParticipants.add(participant);
-		System.out.print(firstName + " " + lastName + " from " + teamName + " with number " + participantNumber + " added");
+		participantNumbers.add(participantNumber);
+		System.out.println(firstName + " " + lastName + " from " + teamName + " with number " + participantNumber 
+				+ " added.");
+	}
+	
+	public void removeParticipant() {
+		
+		boolean removed = false;
+		
+		do {
+			System.out.print("Number: ");
+			removeNumber = scan.nextInt();
+			scan.nextLine();
+			
+			for (int i = 0; i < allParticipants.size(); i++) {
+				Participant tempParticipant = allParticipants.get(i);
+				if (tempParticipant.getParticipantNumber() == removeNumber) {
+					removed = true;
+				}
+				
+				if (removed) {
+					System.out.println(tempParticipant.toStringWithNumber() + " removed.");
+					allParticipants.remove(tempParticipant);
+					break;
+				}
+			}
+			
+			if (!removed) {
+				System.out.println("Error: no participant with number " + removeNumber + " exists.");
+			}
+		} while (removeNumber < 100);
+	}
+	
+	public void participant() {
+		System.out.print("Number: ");
+		searchParticipant = scan.nextInt();
+		scan.nextLine();
+		
+		if (participantNumbers.contains(searchParticipant)) {
+			System.out.print("Event: ");
+			String resultEvent = scan.nextLine();
+		
+		}
+		else {
+			System.out.println("Error: no participant with number " + searchParticipant + " found!");
+		}
 	}
 	
 	public void addResult() {
 		System.out.print("Number: ");
-		participantNumber = scan.nextInt();
+		checkParticipant = scan.nextInt();
 		scan.nextLine();
-		System.out.print("Event: ");
-		String resultEvent = scan.nextLine();
 		
-		for (int i = 0; i < allEvents.size(); i++) {
-			if (allEvents.get(i).getName().equalsIgnoreCase(resultEvent)) {
-				// Obs lägg in kod här för listorna för team och deltagare
-				System.out.print("Results for " + "participantName" + "from " + "teamName" + " in " 
-				+ allEvents.get(i).getName() +": ");
-				resultNumber = scan.nextInt();
-			}
-			else {
-				System.out.println("Error: no event called: '" + allEvents.get(i).getName() + "' found.");
-			}	
+		if (participantNumbers.contains(checkParticipant)) {
+				System.out.print("Event: ");
+				String resultEvent = scan.nextLine();
+				
+				if (eventNames.contains(resultEvent)) {
+						Participant tempoParticipant = allParticipants.get(checkParticipant);
+						System.out.print("Results for " + tempoParticipant.toString() + " in");
+					
+				}
+				else {
+					System.out.println("Error: no event called: '" + resultEvent + "' found!");
+				}	
+		}
+		else {
+			System.out.println("Error: no participant with number " + checkParticipant + " found!");
 		}
 	}
+	
+	public void eventInput() {
+		
+	}
+	
+	public void message(String longText) {
+		System.out.println("############################################################");
+		System.out.println(String.format("%-58s #", "#"));
+		System.out.println(String.format("# %-56.56s #", longText));
+		System.out.println(String.format("%-58s #", "#"));
+		System.out.println("############################################################");
+	}
+	
+	public void sysExit() {
+		System.out.println("Programmet avslutas");
+		System.exit(0);
+		}
 }
