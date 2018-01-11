6281451905a42b5a5f5a42b1d9f0db460d009ee2
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-01-11 19:14:34 +0100

    Update Participant.java

--- Participant.java
+++ Participant.java
@@ -1,17 +1,36 @@
-
 public class Participant {
 	
 	private String firstName, lastName, teamName;
 	private int participantNumber;
 	
 	public Participant(String firstName, String lastName, String teamName, int participantNumber) {
-	
-	this.firstName = firstName; 
-	this.lastName = lastName;
-	this.teamName = teamName;
-	this.participantNumber = participantNumber;
-	
-			
+		this.firstName = firstName; 
+		this.lastName = lastName;
+		this.teamName = teamName;
+		this.participantNumber = participantNumber;
 	}
-
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+	
+	public int getParticipantNumber() {
+		return participantNumber;
+	}
+	
+	public String toString() {
+		return firstName + " " + lastName + " from " + teamName;
+	}
+	
+	public String toStringWithNumber() {
+		return firstName + " " + lastName + " from " + teamName + " with number " + participantNumber;
+	}
 }
