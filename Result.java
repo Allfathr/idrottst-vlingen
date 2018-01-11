9f3249bb51431898b19f098b6bb6e5da1d604b52
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-01-11 19:14:16 +0100

    Update Result.java

--- Result.java
+++ Result.java
@@ -1,3 +1,19 @@
 public class Result {
 	
+	private String eventName, participantName;
+	private double result;
+
+	public Result(String eventName, String participantName, double result) {
+		this.eventName = eventName;
+		this.participantName = participantName;
+		this.result = result;
+	}
+	
+	public String getName() {
+		return participantName;
+	}
+	
+	public String toString() {
+		return String.format("Results for %s in %s: %d", participantName, eventName, result); 
+	}
 }
