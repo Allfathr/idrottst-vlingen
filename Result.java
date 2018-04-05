627b2b17bf0e9e3bb489608d16fed0506025a69b
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-04-05 12:17:07 +0200

    Update Result.java

--- Result.java
+++ Result.java
@@ -1,20 +1,62 @@
+// Anton Sandström ansa6928
+// Jesper Jönsson jeja6606
+
+import java.util.Arrays;
+
 public class Result {
 	
 	private Event event;
 	private Participant participant;
-	private double result;
-	
-	public Result() {
-		
-	}
+	private int tries = 0;
+	// deklarerar en Array med doubles
+	private double[] resultAttempts;
 
 	public Result(Event event, Participant participant, double result) {
 		this.event = event;
 		this.participant = participant;
-		this.result = result;
+		
+		// allokerar plats i arrayen för så många försök grenen har
+		resultAttempts = new double[event.getAttempts()];
+		resultAttempts[tries] = result;
+		tries++;
+	}
+	
+	public double bestResult() {
+		double best = -1;
+		for (int i = 0; i < tries; i++) {
+			if (resultAttempts[i] > best) {
+				best = resultAttempts[i];
+			}
+		}
+		return best;
 	}
 		
 	public String toString() {
-		return String.format("Results for %s in %s: %d", participant.getFullName(), event.getName(), result); 
+		return String.format("Results for %s in %s: %s", participant.getFullName(),
+							event.getName(), Arrays.toString(resultAttempts)); 
+	
 	}
+	
+	public Participant getParticipant() {
+		return participant;
+	}
+	
+	public Event getEvent() {
+		return event;
+	}
+	
+	public int getTries() {
+		return tries;
+	}
+	
+//	public double[] getResultAttempts() {
+//		return resultAttempts;
+//	}
+	
+	public void addTries(double resultTries) {
+		System.out.println(resultAttempts.length);
+		resultAttempts[tries] = resultTries;
+		tries++;
+	}
+	
 }
