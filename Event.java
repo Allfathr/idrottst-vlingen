fa5f5d1d0c349aa9b25a71d8bd18a5de0461a27d
Allfathr <31654921+Allfathr@users.noreply.github.com>
2018-04-05 12:16:20 +0200

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,14 +1,14 @@
+// Anton Sandström ansa6928
+// Jesper Jönsson jeja6606
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Event {
 
 	private ArrayList<Result> allResults = new ArrayList<>();
 	private String name;
 	private int attempts;
-	
-	public Event() {
-		
-	}
 	
 	public Event(String name, int attempts) {
 		this.name = name;
@@ -19,14 +19,57 @@ public class Event {
 		return name;
 	}
 	
+	public int getAttempts() {
+		return attempts;
+	}
+	
 	public void addResult(Result result) {
 		allResults.add(result);
 	}
 	
-	// Glöm ej skriva in [grennamn] uppgift
+	
 	public void printResultList() {
-		System.out.println("Results for " + name);
+		System.out.println(allResults);
 		
 	}
 	
+	public void removePartResult(Participant p) {
+		for (Result r : allResults) {
+			if (r.getParticipant().getParticipantNumber() == p.getParticipantNumber()) {
+				allResults.remove(r);
+				break;
+			}
+		}
+	}
+	
+	public void getHighScore() {
+		sortResult();
+		int placing = 0;
+		double prevResult = -1;
+		for (int i = 0; i < allResults.size(); i++) {
+			if (allResults.get(i).bestResult() != prevResult) {
+				placing = i + 1;
+				prevResult = allResults.get(i).bestResult();
+				
+			}
+			System.out.println(placing + " " + allResults.get(i).bestResult() 
+								+ " " + allResults.get(i).getParticipant());
+		}
+	}
+	
+	// Använder en insertion sort och collection klassen
+	private void sortResult() {
+		for (int i = 1; i < allResults.size(); i++) {
+			for (int j = i; j > 0; j--) {
+				if (allResults.get(j).bestResult() > allResults.get(j - 1).bestResult()) {
+					Collections.swap(allResults, (j - 1), j);
+				}
+				else if (allResults.get(j).bestResult() == allResults.get(j - 1).bestResult()) {
+					if (allResults.get(j).getParticipant().getFullName().compareTo(allResults.get(j - 1).getParticipant().getFullName()) < 0) {
+						Collections.swap(allResults, (j - 1), j);
+					}
+				}
+			}
+		}
+	}
 }
